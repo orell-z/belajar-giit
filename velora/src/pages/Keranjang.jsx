4ce1c6b6581c14a67249c5b9570d6806de3e0bd9
muhamad rafy altoriq <mrafialtoriq@gmail.com>
2026-07-03 23:27:31 +0700

    perbaikan di keranjang

--- velora/src/pages/Keranjang.jsx
+++ velora/src/pages/Keranjang.jsx
@@ -2,17 +2,9 @@ const Keranjang = () => {
   return (
     <div className="bg-black min-h-screen text-white">
 
-      <nav className="flex items-center justify-between px-10 py-6 border-b border-gray-800">
+      <nav className="flex items-center justify-between px-10 py-6">
 
         <div className="flex w-[70%]">
-          <input
-            type="text"
-            placeholder="Cari parfum favoritmu"
-            className="w-full bg-black border border-yellow-500 rounded-l-3xl px-8 py-5 text-2xl text-white outline-none"
-          />
-          <button className="bg-yellow-500 text-black font-bold text-2xl px-12 rounded-r-3xl hover:bg-yellow-400 transition">
-            Cari
-          </button>
         </div>
 
       </nav>
@@ -30,12 +22,45 @@ const Keranjang = () => {
     <p className="mt-5 text-2xl text-gray-300">
       Yuk pilih parfum premium favoritmu di Velora
     </p>
-    
-    <button className="mt-14 bg-yellow-500 text-black font-bold text-3xl px-16 py-6 rounded-3xl hover:bg-yellow-400 transition">
-          Belanja Sekarang
-    </button>
 
   </section>
+<div className="fixed bottom-0 left-0 w-full bg-[#121212] border-t border-yellow-500 shadow-2xl">
+  <div className="max-w-7xl mx-auto flex items-center justify-between px-8 py-5">
+
+    <div className="flex items-center gap-4">
+      <input
+        type="checkbox"
+        className="w-6 h-6 accent-yellow-500"
+      />
+
+      <span className="text-white text-2xl font-semibold">
+        Semua
+      </span>
+    </div>
+
+    <div className="flex items-center gap-8">
+
+      <div className="text-right">
+        <p className="text-gray-400 text-sm">
+          Total
+        </p>
+
+        <h2 className="text-yellow-500 text-4xl font-bold">
+          Rp0
+        </h2>
+      </div>
+
+      <button
+        className="
+          bg-yellow-500 hover:bg-yellow-400 text-black font-bold text-2xl px-12 py-5 rounded-2xl transition"
+      >
+        Checkout (0)
+      </button>
+
+    </div>
+
+  </div>
+</div>
     </div>
   );
 };
